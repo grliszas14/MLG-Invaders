791ba2e331807b25eb7c9e673da6e7a6777a4c94
grliszas14 <grliszas14@gmail.com>
2016-03-24 20:14:58 +0100

    Klasa Sounds do odtwarzania dźwięków

---
+++ MLG Invaders/src/Sounds.java
@@ -0,0 +1,21 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sounds {
+
+	public static synchronized void playSound(final String url) {
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream inputStream = AudioSystem.getAudioInputStream(Sounds.class.getResource(url));
+					clip.open(inputStream);
+					clip.start();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		}).start();
+	}
+}
