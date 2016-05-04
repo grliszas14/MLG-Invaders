7e4e168ce7208c0b778d9f22ff9ba85860b0c521
Grzegorz Wojciechowski <grliszas14@gmail.com>
2016-05-04 15:09:05 +0200

    Aktualizacja panelu ( moc)

--- MLG Invaders/src/SidePanel.java
+++ MLG Invaders/src/SidePanel.java
@@ -6,8 +6,8 @@ import java.awt.image.*;
 public class SidePanel extends JPanel {
 	
 	private BufferedImage image;
-	private int score = 0;
-	private int lives = 0;
+	private int score;
+	private int livesSidePanel = 0;
 	private Graphics2D g;
 	public static int HEIGHT = 	Integer.parseInt(Config.getProperties().getProperty("SidePanelHeight"));	
 	public static int WIDTH = 	Integer.parseInt(Config.getProperties().getProperty("SidePanelWidth"));
@@ -16,7 +16,7 @@ public class SidePanel extends JPanel {
 	
 	public SidePanel(){
 		super();
-		setPreferredSize(new Dimension(200, 590));
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		this.setBackground(Color.WHITE);
 		setVisible(true);
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -32,7 +32,8 @@ public class SidePanel extends JPanel {
 		String s = "Wynik: ";
 		String l = "Zycia:";
 		String p = Integer.toString(score);
-		String h = Integer.toString(lives);
+		String h = Integer.toString(livesSidePanel);
+		String pu = "Moc: ";
 		
 		g.setColor(Color.BLACK);
 		g.setFont(new Font("Century Gothic", Font.PLAIN, 18));
@@ -40,10 +41,45 @@ public class SidePanel extends JPanel {
 		int lengthl = (int) g.getFontMetrics().getStringBounds(l, g).getWidth();
 		int lengthp = (int) g.getFontMetrics().getStringBounds(p, g).getWidth();
 		int lengthh = (int) g.getFontMetrics().getStringBounds(h, g).getWidth();
-		g.drawString(s, (WIDTH - lengths)/ 2, HEIGHT / 8);
-		g.drawString(p, (WIDTH - lengthp)/ 2, 2 * HEIGHT / 8);
-		g.drawString(l, (WIDTH - lengthl)/ 2, 5* HEIGHT /8);
-		g.drawString(h, (WIDTH - lengthh)/ 2, 6* HEIGHT /8);
+		g.drawString(s, (WIDTH - lengths)/ 2, HEIGHT / 12);
+		g.drawString(p, (WIDTH - lengthp)/ 2, 2 * HEIGHT / 12);
+		
+		//g.drawString(player.getScore(), (WIDTH - lengthp)/ 2, 2 * HEIGHT / 8);
+		
+		g.drawString(l, (WIDTH - lengthl)/ 2, 8* HEIGHT /12);
+		g.drawString(pu,(WIDTH - lengthl)/ 2, 5* HEIGHT /12);
+		
+		//tymczasowe rysowanie mocy
+		for(int i = 0; i < 5; i++){
+			g.setColor(Color.YELLOW);
+			g.fillRect(50 + (20 * i), 6 * HEIGHT / 12, (int) 10 * 2, (int) 10 * 2);
+			((Graphics2D) g).setStroke( new BasicStroke(3));
+			g.setColor(Color.YELLOW.darker());
+			g.drawRect(50 + (20 * i), 6 * HEIGHT / 12, (int) 10 * 2, (int) 10 * 2);
+			((Graphics2D) g).setStroke( new BasicStroke(1));
+		}
+		
+		/* rysowanie mocy powinno wygladac tak:
+		 * g.setColor(Color.YELLOW);
+		 * g.fillRect(50 + (20 * i), 6 * HEIGHT / 12, player.getPower() * 8 , 8);
+		 * g.setColor(Color.YELLOW.darker());
+		 * g.setStroke(new BasicStroke(2));
+		 * for( int i = 0; i < player.getRequiredPower(); i++){
+		 * 	g.drawRect(50 + 8 *i,  6 * HEIGHT / 12, 8,8);
+		 * }
+		 */
+		
+		// tu zamiast 3 ma byc player.getLives();
+		for(int i = 0; i < 3; i++){
+			g.setColor(Color.WHITE);
+			g.fillOval(70 + (20 * i), 9 * HEIGHT / 12, (int) 10 * 2, (int) 10 * 2);
+			((Graphics2D) g).setStroke( new BasicStroke(3));
+			g.setColor(Color.WHITE.darker());
+			g.drawOval(70 + (20 * i), 9 * HEIGHT / 12, (int) 10 * 2, (int) 10 * 2);
+			((Graphics2D) g).setStroke( new BasicStroke(1));
+		}
+		
+		
 		
 	}
 }
