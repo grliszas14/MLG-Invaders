cd8fc502b1891dcf2fae4463151ebf16e339882a
Grzegorz Wojciechowski <grliszas14@gmail.com>
2016-06-19 22:00:33 +0200

    Commented engine

--- MLG Invaders/src/Engine.java
+++ MLG Invaders/src/Engine.java
@@ -1,17 +1,46 @@
 import java.awt.Graphics2D;
-
+/**
+ * Class that enemy, bullet, bomb, explosion inherit from
+ */
 public class Engine {
-	
+	/**
+	 *  Coordinate x
+	 */
 	private double x;
+	/**
+	 * Coordinate y
+	 */
 	private double y;
+	/**
+	 * Radius
+	 */
 	private int r;
 	
-	
+	/**
+	 * Method to draw
+	 * @param g
+	 */
 	public void draw(Graphics2D g){}
+	/**
+	 * Method to update the state of object
+	 * @return
+	 */
 	public boolean 	update() { return false;}
 	
+	/**
+	 * x getter
+	 * @return x
+	 */
 	public double getx() { return x;}
+	/**
+	 * y getter
+	 * @return y
+	 */
 	public double gety() { return y;}
+	/**
+	 * radius getter
+	 * @return r
+	 */
 	public double getr() { return r;}
 	
 	
