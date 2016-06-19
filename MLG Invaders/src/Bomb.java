be1ecbc2f24db0b6d5acaff53b86295df0780c36
Grzegorz Wojciechowski <grliszas14@gmail.com>
2016-06-19 21:35:14 +0200

    Commented bomb

--- MLG Invaders/src/Bomb.java
+++ MLG Invaders/src/Bomb.java
@@ -3,21 +3,38 @@ import java.awt.*;
 import javax.swing.ImageIcon;
 
 public class Bomb extends Engine{
-
+	/**
+	 * Coordinate x
+	 */
 	private double x;
+	/**
+	 * Coordinate y
+	 */
 	private double y;
+	/**
+	 * Radius of bomb
+	 */
 	private int r;
-	private Color color1;
-	private Color color2;
-	
+
+	/**
+	 * x getter
+	 */
 	public double getx() { return x;}
+	/**
+	 * y getter
+	 */
 	public double gety() { return y;}
+	/**
+	 * radius getter
+	 */
 	public double getr() { return r;}
-	
+	/**
+	 * Image of a bomb
+	 */
 	private Image bombka;
 	
 	/**
-	 * Tworzy bombe, jej grafike, ustawia jej rozmiar i polozenie
+	 * Creates a bomb
 	 * @param x
 	 * @param y
 	 */
@@ -25,14 +42,11 @@ public class Bomb extends Engine{
 		bombka = new ImageIcon(Config.getProperties().getProperty("Bombka")).getImage();
 		this.x = x;
 		this.y = y + 25;
-		
-		color1 = Color.BLACK;
-		color2 = Color.RED;
 		r = 10;
 	}
 	
 	/**
-	 * zmienia polozenie bomby
+	 * Updates position of a bomb
 	 */
 	public boolean update() {
 		y += 2;
@@ -45,15 +59,12 @@ public class Bomb extends Engine{
 	}
 	
 	/**
-	 * rysuje bombe 
+	 * Method drawing a bomb
+	 * @param g to draw bomb
+	 * @param factorWidth variable to resize bomb
+	 * @param factorHeight variable to resize bomb
 	 */
 	public void draw(Graphics2D g, double factorWidth, double factorHeight){
-		//g.setColor(color1);
-		//g.fillOval((int) x - r, (int) y - r + 50, 2 * r, 2 * r);
-		//g.setStroke(new BasicStroke(3));
-		//g.setColor(color2);
-		//g.drawOval((int) x - r, (int) y - r + 50, 2 * r, 2 * r);
-		//g.setStroke(new BasicStroke(1));
 		g.drawImage(bombka, (int) ((x - r)*factorWidth), (int) ((y-r)*factorHeight), (int) ( 25*factorWidth), (int) (19*factorHeight), null);
 	}
 	
